d4ee7641001baa405ad3b525c68c9e6893e8719a
michael mapesa <michaelmapesa93@gmail.com>
2022-02-17 19:56:52 +0300

    modify file

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Quote } from '../quote';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input() quoty!: Quote
+  @Output() isRead= new EventEmitter<boolean>();
+  deleteQuote(read:boolean){
+    this.isRead.emit(read);
+  }
+  upvote(){
+    this.quoty.likes+=1;
+  }
+  downvote(){
+    this.quoty.dislikes+=1;
+  }
 
   constructor() { }
 
