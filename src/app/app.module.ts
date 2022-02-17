94afd562ca430561cdfa7370d2496168847d0f20
michael mapesa <michaelmapesa93@gmail.com>
2022-02-17 19:58:42 +0300

    modify files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { ChangequoteDirective } from './changequote.directive';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     ChangequoteDirective,
-    QuoteFormComponent
+    QuoteFormComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from '@angular/compiler';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -9,21 +9,21 @@ import { Quote } from '@angular/compiler';
 export class QuoteComponent implements OnInit {
   title='Begin with quote app!'
   quotes:Quote[]=[
-    new Quote(1,'Michael','motivation','Yes we can.','Barrack Obama', new Date(2019,7,12),0,0)
-    new Quote(2,'Michael','Inspiring','Forever young.','Jayz',new Date(2017,8,8)0,0)
+    new Quote(1,'Michael','motivation','Yes we can.','Barrack Obama', new Date(2019,7,12),0,0),
+    new Quote(2,'Michael','Inspiring','Forever young.','Jayz',new Date(2017,8,8),0,0)
   ];
   get sortQuotes(){
     return this.quotes.sort((a, b) => {
       return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
     });
   }
-  addedQuote(quote){
+  addedQuote(quote:any){
     let arraysize = this.quotes.length;
     quote.id=arraysize+1;
     quote.completeDate=new Date(quote.completeDate)
-    this.quotes.push
+    this.quotes.push(quote)
   }
-  quoteDelete(isReady, index){
+  quoteDelete(isReady:any, index:any){
     if (isReady){
       let toDelete= confirm("Do you really want to delete?")
       if(toDelete){
@@ -31,7 +31,7 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
-  displayInfo(index){
+  displayInfo(index:number){
     this.quotes[index].showInfo=!this.quotes[index].showInfo;
   }
   
